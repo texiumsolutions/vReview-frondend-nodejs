ec1f74414d4f6c5c5667ad18b272428046a908aa
Bharatsiddharth <134818090+Bharatsiddharth@users.noreply.github.com>
2026-01-18 23:46:54 +0530

    db fix

--- config/db.js
+++ config/db.js
@@ -2,13 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://arsh001356_db_user:KwHqOknnOR1REDRC@user.je5xlkv.mongodb.net/?retryWrites=true&w=majority&appName=user/yourDatabaseName', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB Connected...');
+        await mongoose.connect('mongodb+srv://omixflow:aDwrgsUfDCfFzdsg@cluster0.o3jj6wy.mongodb.net/yourDatabaseName?retryWrites=true&w=majority&appName=Cluster0');
+        console.log('✅ MongoDB Connected Successfully!');
     } catch (err) {
-        console.error(err.message);
+        console.error('❌ MongoDB Connection Error:', err.message);
         process.exit(1);
     }
 };
